61efe036f01b994c7be83136960afd73c4bce9f0
SteveCalvin <stevekebin@gmail.com>
2026-04-27 18:36:03 +0800

    fix: switch all routes to client render mode

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,27 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'submit/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'student-feedback/:id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'review-details/:assignment_id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'review-approval/:feedback_id/:submission_id',
-    renderMode: RenderMode.Server
-  },
-  {
-    path: 'edit-assignment-form/:id',
-    renderMode: RenderMode.Server
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Client
   }
 ];
